b39251dd8a5dd310022abbf9d333e8ed7f4e4bf4
tiouoo <tiouo@qq.com>
2026-08-18 16:02:00 +0800

    Adjust help layout and add usage hint

--- src/commands/help.js
+++ src/commands/help.js
@@ -3,12 +3,13 @@ export const help = {
   description: '显示本菜单',
   async execute({ replyMarkdown, commands }) {
     const lines = [
-      '# 可用指令',
+      '### 可用指令',
       '',
       ...commands.flatMap((cmd) => {
-        const name = `**${cmd.names.join(' / ')}**`;
+        const name = `${cmd.names.join(' / ')}`;
         return cmd.description ? [name, `> ${cmd.description}`, ''] : [name, ''];
       }),
+      '提示：可用 . / 。 \\ 触发命令，如 /help'
     ];
     await replyMarkdown(lines.join('\n'));
   },
